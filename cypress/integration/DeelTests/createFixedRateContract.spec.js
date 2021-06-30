87cb3ad06dff489f2654b744cd4630d8a6a7bb3e
Pablo Lopez <lop14509@uvg.edu.gt>
2021-06-30 15:21:10 -0600

    Add files via upload

--- cypress/integration/DeelTests/createFixedRateContract.spec.js
+++ cypress/integration/DeelTests/createFixedRateContract.spec.js
@@ -12,28 +12,22 @@ describe('Contracts', () => {
     const homePage = new HomePage();
     const create = new Create();
     const fixedRate = new FixedRate();
+    let data;
 
-    let email = 'pj9lopez@hotmail.com';
-    let password = 'Pablodeeltest1*';
-    let contractName = "Pablo's Contract";
-    let scope = "This is a test";
-    let rate = "1000";
-    let currency = "GBP - British Pound";
-    let per = "Week"; 
-    let specialClause = "Special Clause:  this is a test";
-    let country = "United States";
-    let state = "Colorado"
 
     // Opens the browser in the login page before starting each of the tests on the suite.
     beforeEach(() => {
-      cy.visit('https://app.letsdeel.com/login')
+      cy.visit('https://app.letsdeel.com/login');
+      cy.fixture('fixedRateData').then((frData) => {
+        data = frData;
+      });
     });
   
     it('Create Fixed Rate Contract', () => {
-      loginPage.login(email, password);
+      loginPage.login(data.email, data.password);
       homePage.clickPopUpCloseButton();
       sideBar.clickCreateAContract();
       create.clickCreateFixedRate();
-      fixedRate.createFixedRate(contractName, scope, rate, currency, per, specialClause, country, state);
+      fixedRate.createFixedRate(data.contractName, data.scope, data.rate, data.currency, data.per, data.specialClause, data.country, data.state);
     });
 });
--- cypress/integration/DeelTests/login.spec.js
+++ cypress/integration/DeelTests/login.spec.js
@@ -3,22 +3,21 @@ import LoginPage from "../DeelPOMs/LoginPage.js";
 
 describe('Login', () => {
     const loginPage = new LoginPage();
-
-    let email = 'pj9lopez@hotmail.com';
-    let password = 'Pablodeeltest1*';
-    let testEmail  = 'test';
-    let testPassword = 'test';
+    let user;
 
     // Opens the browser in the login page before starting each of the tests on the suite.
     beforeEach(() => {
-      cy.visit('https://app.letsdeel.com/login')
+      cy.visit('https://app.letsdeel.com/login');
+      cy.fixture('loginData').then((data) => {
+        user = data;
+      });
     });
   
     it('Successfull Login', () => {
-      loginPage.login(email, password);
+      loginPage.login(user.email, user.password);
     });
 
     it('Invalid Login', () => {
-      loginPage.wrongLogin(testEmail, testPassword);
+      loginPage.wrongLogin(user.wrongEmail, user.wrongPassword);
     });
 });
